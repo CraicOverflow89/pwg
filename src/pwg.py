56c9a37e457e39328c1c0461d59ced2852ab508b
CraicOverflow89 <craic.overflow@hotmail.com>
2020-08-25 22:29:11 +0100

    Random first and third chars.

--- src/pwg.py
+++ src/pwg.py
@@ -1,4 +1,9 @@
 from random import randrange
 from uuid import uuid4
 
-print(f"j{str(randrange(10))}#" + str(uuid4()).replace("-", "").upper()[:12])
+print("".join([
+	chr(randrange(97, 123)),
+	str(randrange(10)),
+	chr([33, 35, 36, 37, 38][randrange(5)]),
+	str(uuid4()).replace("-", "").upper()[-12:]
+]))
