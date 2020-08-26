ba9368f9caa2a8aa58d081fe0e79b7ff1a3704a0
CraicOverflow89 <craic.overflow@hotmail.com>
2020-08-26 18:43:16 +0100

    Inserting lower/digit/special chars in random positions.

--- src/pwg.py
+++ src/pwg.py
@@ -1,9 +1,12 @@
+from functools import reduce
 from random import randrange
 from uuid import uuid4
 
-print("".join([
+def insertCharAt(string, character, position):
+	return string[:position] + character + string[position:]
+
+print(reduce(lambda v, it: insertCharAt(v, it, randrange(len(v))), [
 	chr(randrange(97, 123)),
 	str(randrange(10)),
-	chr([33, 35, 36, 37, 38][randrange(5)]),
-	str(uuid4()).replace("-", "").upper()[-12:]
-]))
+	chr([33, 35, 36, 37, 38][randrange(5)])
+], str(uuid4()).replace("-", "").upper()[-12:]))
