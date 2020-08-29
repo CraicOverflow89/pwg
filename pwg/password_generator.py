5e369a57e09dd592ee588cd93e44cca51060b71c
CraicOverflow89 <craic.overflow@hotmail.com>
2020-08-29 18:32:12 +0100

    Explicit uppercase char, docstrings, mypy types.

--- pwg/password_generator.py
+++ pwg/password_generator.py
@@ -5,16 +5,29 @@ from uuid import uuid4
 
 class PasswordGenerator:
     @staticmethod
-    def create():
-        def insertCharAt(string, character, position):
+    def create() -> str:
+        """
+        Creates a random password
+        :return: string of fifteen characters
+        """
+
+        def insertCharAt(string: str, character: str, position: int) -> str:
+            """
+            Inserts a character at a position into a specific string
+            :param string: the string to insert a character into
+            :param character: the string to insert
+            :param position: the position at which to insert the character into the string
+            :return: new string with inserted character
+            """
             return string[:position] + character + string[position:]
 
         return reduce(
             lambda v, it: insertCharAt(v, it, randrange(len(v))),
             [
                 chr(randrange(97, 123)),
+                chr(randrange(65, 91)),
                 str(randrange(10)),
                 chr([33, 35, 36, 37, 38][randrange(5)]),
             ],
-            str(uuid4()).replace("-", "").upper()[-12:],
+            str(uuid4()).replace("-", "").upper()[-11:],
         )
